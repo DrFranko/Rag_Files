45057ccc68c1b2f21b874019d7b5b0246194d531
Rohit <frankenstein0671@gmail.com>
2024-08-20 17:01:09 +0530

    Second Commit

--- Rag_Graph/graph.py
+++ Rag_Graph/graph.py
@@ -1,1 +1,10 @@
-import langchain
+from dotenv import load_dotenv
+import os
+from langchain_community.llms import ollama
+
+load_dotenv()
+
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+
+llm=ollama(model="llama2")
